8bc9fa9c9731bcc171edc5a4af67924b7592e452
monica q <monicaq@MacBook-Pro-383.local>
2023-05-12 18:15:53 -0700

    Add user to mongo atlas db

--- seeds/index.js
+++ seeds/index.js
@@ -29,7 +29,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
-      author: "644c593c1d6351cc7b0bdcdf",
+      author: "645ee3f10ef250e2dbcfa3e0",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       geometry: {
